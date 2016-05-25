cf5c7ced2b44cacf4dd36608b3c17f1dcd8fe4f6
Abdurrahman S <a.sibahi@gmail.com>
2016-05-24 21:22:35 -0500

    First commit

---
+++ ClassLibrary1/LogAnalyzerTests.cs
@@ -0,0 +1,32 @@
+﻿using NUnit.Framework;
+
+namespace LogAn.UnitTests
+{
+	[TestFixture]
+	public class LogAnalyzerTests
+	{
+		[Test]
+		public void IsValidLogFileName_BadExtension_ReturnsFaslse()
+		{
+			var analyzer = new LogAnalyzer();
+			var result = analyzer.IsValidLogFileName("filewithbadextension.foo");
+			Assert.False(result);
+		}
+
+		[Test]
+		public void IsValidLogFileName_GoodExtensionLowercase_ReturnsTrue()
+		{
+			var analyzer = new LogAnalyzer();
+			var result = analyzer.IsValidLogFileName("filewithgoodextension.slf");
+			Assert.True(result);
+		}
+
+		[Test]
+		public void IsValidLogFileName_GoodExtensionUppercase_ReturnsTrue()
+		{
+			var analyzer = new LogAnalyzer();
+			var result = analyzer.IsValidLogFileName("filewithgoodextension.SLF");
+			Assert.True(result);
+		}
+	}
+}
---
+++ LogAn/LogAnalyzer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LogAn
+{
+	public class LogAnalyzer
+	{
+		public bool IsValidLogFileName(string fileName) => fileName.EndsWith(".SLF") ? true : false;
+	}
+}
